d90c6c6d595b36748f88166bad7a14f6a3bb9155
James Adam Connor <j.adam.connor@gmail.com>
2021-12-17 00:59:23 -0600

    conditional rendering of category with router.isFallback?

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,16 +1,27 @@
 
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
 import { getAllCategories, getPostsByCategory } from '../../lib/api'
 
 export default function Categories({ posts }) {
+	const router = useRouter()
+
+	if (!router.isFallback && !posts?.edges) {
+	  return <ErrorPage statusCode={404} />
+	}
 	return (
 		<>
-			{posts?.edges.map((post) => (
+		{router.isFallback ? (
+			<div>Loading…</div>
+		  ) : (
+			posts.edges.map((post) => (
 				<div key={post.node.id}>
 					<a href={`../${post.node.slug}`}>
 						{post.node.title}
 					</a>
 				</div>
-			))}
+			))
+		  )}
 		</>
 	)
 }
