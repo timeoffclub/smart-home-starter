56408de30c0f763bd05a6bcee6f86a9d173c16b2
James Adam Connor <j.adam.connor@gmail.com>
2021-12-22 00:47:46 -0600

    generating all articles at build time with fallback: false

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,44 +1,54 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../lib/api'
+import { getPostsWithSlug, getPostAndMorePosts } from '../lib/api'
 
 export default function Post({ post, posts, preview }) {
-  const router = useRouter()
 
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
-  }
-
-  return (
-      <>
-      {router.isFallback ? (
-        <div>Loading…</div>
-      ) : (
-        <div dangerouslySetInnerHTML={{__html: post.content}}></div>
-      )}
-      </>
-  )
+    const router = useRouter()
+    
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage statusCode={404} />
+    }
+    
+    return (
+        <>
+        {router.isFallback ? (
+                <div>Loading…</div>
+            ) : (
+                <div dangerouslySetInnerHTML={{__html: post.content}}></div>
+            )}
+        </>
+    )
 }
-
+            
 export async function getStaticProps({ params, preview = false, previewData }) {
-  const data = await getPostAndMorePosts(params.slug, preview, previewData)
-
-  return {
-    props: {
-      preview,
-      post: data.post,
-      posts: data.posts,
-    },
-    revalidate: 1
-  }
-
+    const data = await getPostAndMorePosts(params.slug, preview, previewData)
+    
+    return {
+        props: {
+            preview,
+            post: data.post,
+            posts: data.posts,
+        },
+        revalidate: 1
+    }
+    
 }
 
 export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug()
-
-  return {
-    paths: allPosts.edges.map(({ node }) => `/${node.slug}`) || [],
-    fallback: false
-  }
+    let data = []
+    let endCursor = null
+    let hasNextPage = true
+    let i = 0
+    do {
+        let res = await getPostsWithSlug(endCursor || null)
+        endCursor = await res?.pageInfo.endCursor
+        hasNextPage = await res?.pageInfo.hasNextPage
+        data.push(res)
+    } while (hasNextPage)
+    
+    return {
+        paths: data[0].edges.map(({ node }) => `/${node.slug}`) || [],
+        fallback: false
+    }
 }
