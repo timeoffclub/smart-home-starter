5e13a8fbd2b112776c9f4018e242cc43c49253a0
James Adam Connor <j.adam.connor@gmail.com>
2021-12-31 22:13:24 -0600

    fixed bugger more... button in filter bar

--- components/article-filter-bar.js
+++ components/article-filter-bar.js
@@ -2,27 +2,24 @@ import { useState } from 'react'
 
 export default function ArticleFilterBar ({ myMenu, myCategory, onFilter }) {
     const tabsPaginate = 7;
-    const [moreTabs, setMoreTabs] = useState(
-        "hidden"
-    )
+    const [moreTabs, setMoreTabs] = useState(false)
+
     const toggleMoreTabs = () => {
-        moreTabs === "hidden" ? setMoreTabs("block") : setMoreTabs("hidden")
+        setMoreTabs(!moreTabs)
     }
-    const [activeTab, setActiveTab] = useState(
-        "All"
-    )
+    const [activeTab, setActiveTab] = useState('All')
     return (
         <>
-            <div className="container mb-5 px-6 xl:px-0">
+            <div className='container mb-5 px-6 xl:px-0'>
                 <div className='font-semibold text-2xl mb-10 tracking-wider'>
                     More {myCategory} Articles
                 </div>
-                <div onClick={() => {onFilter("All"); setActiveTab("All"); setMoreTabs("hidden")}} className={activeTab !== "All" ? `inline font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `inline font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
+                <div onClick={() => {onFilter('All'); setActiveTab('All'); setMoreTabs(false)}} className={activeTab !== 'All' ? `inline font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `inline font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
                     All
                 </div>
                 {myMenu.slice(0,tabsPaginate).map((tab) => (
                     tab.label !== myCategory &&
-                    <div onClick={() => {onFilter(tab.label); setActiveTab(tab.label); setMoreTabs("hidden")}} key={tab.label} className={activeTab !== tab.label ? `inline-block font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `inline font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
+                    <div onClick={() => {onFilter(tab.label); setActiveTab(tab.label); setMoreTabs(false)}} key={tab.label} className={activeTab !== tab.label ? `inline-block font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `inline font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
                         {tab.label}
                     </div>
                 ))}
@@ -31,10 +28,10 @@ export default function ArticleFilterBar ({ myMenu, myCategory, onFilter }) {
                         <div onClick={() => toggleMoreTabs()} className='font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer'>
                             More...
                         </div>
-                        <div className={`absolute bg-white z-10 ${moreTabs}`}>
+                        <div className={moreTabs ?  'absolute bg-white z-10' : 'hidden'}>
                             {myMenu.slice(tabsPaginate).map((tab) => (
                                 tab.label !== myCategory &&
-                                <option onClick={() => {toggleMoreTabs(); onFilter(tab.label); setActiveTab(tab.label)}} style={{display: moreTabs}} key={tab.label} className={activeTab !== tab.label ? `inline-block font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `inline-block font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
+                                <option onClick={() => {toggleMoreTabs(); onFilter(tab.label); setActiveTab(tab.label)}} style={{display: moreTabs}} key={tab.label} className={activeTab !== tab.label ? `block font-semibold text-xl tracking-wider px-5 py-1 cursor-pointer` : `block font-semibold text-xl text-white bg-black tracking-wider px-5 py-1 cursor-pointer`}>
                                     {tab.label}
                                 </option>
                             ))}
