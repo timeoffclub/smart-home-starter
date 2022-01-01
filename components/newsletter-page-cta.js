6d3aff34c1ce4688552786b909c96fd666176e92
James Adam Connor <j.adam.connor@gmail.com>
2021-12-31 20:30:11 -0600

    responsiveness tweaks to home-featured component

--- components/newsletter-page-cta.js
+++ components/newsletter-page-cta.js
@@ -3,11 +3,11 @@ import Newsletter from '../components/newsletter'
 export default function NewsLetterPageCTA() {
     
     return (
-        <div className='flex justify-center bg-black py-12 px-5 sm-px-0'>
-            <div className='text-sky-600 w-80 text-2xl font-semibold tracking-wider'>
+        <div className='flex flex-wrap justify-center bg-black py-12 px-5 sm-px-0'>
+            <div className='text-sky-600 w-full md:w-80 text-2xl font-semibold tracking-wider mb-6 md:mb-0'>
                 Sign up for our newsletter for news, deals, and other stuff
             </div>
-            <div className='w-72 ml-5'>
+            <div className='w-full md:w-72 md:ml-5'>
                 <Newsletter />
             </div>
         </div>
