953eb3a44d93b51e2690f3139f4316ed1b4d5b2f
jadamconnor <j.adam.connor@gmail.com>
2022-01-11 13:50:02 -0600

    increased sidebar ad height

--- pages/[slug].js
+++ pages/[slug].js
@@ -121,7 +121,7 @@ export default function Post({ post, related, posts, preview, navigationMenus })
                     </div>
                 </div>
                 <div className='hidden lg:inline col-span-1'>
-                    <div className='sidebar-ad w-full h-[300px] mb-14'>
+                    <div className='sidebar-ad w-full h-[600px] mb-14'>
                     </div>
                     <div className='text-3xl font-semibold mb-5'>
                         Related Articles
@@ -149,7 +149,7 @@ export default function Post({ post, related, posts, preview, navigationMenus })
                         </div>
                         <Newsletter mode={'light'}/>
                     </div>
-                    <div className='sidebar-ad-sticky w-full h-[300px] top-10 sticky'>
+                    <div className='sidebar-ad-sticky w-full h-[600px] top-10 sticky'>
                     </div>
                 </div>
             </div>
