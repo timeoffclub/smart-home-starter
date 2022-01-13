3bce23438af5ac2c68794c4ce222e4d874380f63
jadamconnor <j.adam.connor@gmail.com>
2022-01-13 11:32:24 -0600

    removed mode: jit from tailwind.config (doesn't work) and dynamically import mobilenav

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import Router from 'next/router'
-import SearchForm from './search-form'
+import dynamic from 'next/dynamic'
 import { FaFacebookSquare } from '@react-icons/all-files/fa/FaFacebookSquare'
 import { FaTwitterSquare } from '@react-icons/all-files/fa/FaTwitterSquare'
 import { FaInstagramSquare } from '@react-icons/all-files/fa/FaInstagramSquare'
@@ -9,9 +9,11 @@ import { FaWindowClose } from '@react-icons/all-files/fa/FaWindowClose'
 import { FaBars } from '@react-icons/all-files/fa/FaBars'
 import { FaSearch } from '@react-icons/all-files/fa/FaSearch'
 import { FaTimes } from '@react-icons/all-files/fa/FaTimes'
-import Accordion from './accordian'
 import { kebabCase } from '../lib/utils'
-import NewsletterModal from './newsletter-modal'
+
+const MobileNav = dynamic(() => import('./mobile-nav'))
+const SearchForm = dynamic(() => import('./search-form'))
+const NewsletterModal = dynamic(() => import('./newsletter-modal'))
 
 export default function Header({ menu, slug }) {
 
@@ -151,48 +153,18 @@ export default function Header({ menu, slug }) {
                 />
             </div>
             {/* Mobile nav - beneath main nav on mobile viewports */}
-            <div
-                className={
-                    mobileNav ? 
-                        // If slug is /search bump this menu up higher with top attribute
-                        slug === 'search' ? 
-                            'absolute lg:hidden z-50 left-0 w-screen h-fit bg-black transition-all ease-in-out duration-400 py-12' 
-                        :
-                            'absolute lg:hidden z-50 top-20 left-0 w-screen h-fit bg-black transition-all ease-in-out duration-400 py-12'
-                    :
-                        'absolute lg:hidden w-screen z-50 h-0 bg-black transition-all ease-in-out duration-400'
-                    }
-                >
-                {/* Mobile search - beneath main nav on mobile viewports, unless slug is /search */}
-                <div className={mobileSearchInput ? 'block' : 'hidden'}>
-                    <div>
-                        <label className='text-smart-blue flex lg:hidden justify-center items-center w-full h-24 bg-neutral-900  px-6 md:px-10'>
-                            <SearchForm
-                                searchQuery={ searchQuery }
-                                setSearchQuery={ setSearchQuery }
-                                handleSearchFormSubmit={handleSearchFormSubmit}
-                                mode={'dark'}
-                            />
-                        </label>
-                    </div>
-                </div>
-                {menu.map((el) => (
-                    <div className={mobileNav ? 'px-6 bg-black ' : 'hidden'} key={el.id}>
-                        <Accordion primary={el.name} secondary={el.menuItems.nodes} onToggleNav={toggleMobileNav}/>
-                    </div>
-                ))}
-                <div
-                    onClick={() => {setMobileNav(false), setModalOpen(true)}} 
-                    className={mobileNav ? 'text-smart-blue px-6 text-2xl mt-6 cursor-pointer' : 'hidden'}
-                >
-                    Subscribe
-                </div>
-                <div  className={mobileNav ? 'text-smart-blue px-6 text-2xl mt-6 cursor-pointer' : 'hidden'}>
-                    <a href={'/contact-us'}>
-                        Contact Us
-                    </a>
-                </div>
-            </div>
+            <MobileNav 
+                menu={menu} 
+                slug={slug}
+                mobileNav={mobileNav} 
+                mobileSearchInput={mobileSearchInput} 
+                onSetMobileNav={setMobileNav} 
+                onSetModalOpen={setModalOpen}
+                searchQuery={searchQuery}
+                onSetSearchQuery={setSearchQuery}
+                onHandleSearchFormSubmit={handleSearchFormSubmit}
+                onToggleMobileNav={toggleMobileNav}
+            />
             {/* Megamenu - large viewports */}
             {(megaMenu && !megaMenuDisabled) &&
                 <div className='absolute hidden w-full lg:block bg-black z-40 py-5' onMouseLeave={() => {setMegaMenu(null), setActiveLabel(null)}}>
---
+++ components/mobile-nav.js
@@ -0,0 +1,51 @@
+import dynamic from 'next/dynamic'
+import Accordion from './accordian'
+
+const SearchForm = dynamic(() => import('./search-form'))
+
+export default function MobileNav({ menu, slug, mobileNav, mobileSearchInput, onSetMobileNav, onSetModalOpen, searchQuery, onSetSearchQuery, onHandleSearchFormSubmit, onToggleMobileNav }) {
+    return (
+        <div
+            className={
+                mobileNav ? 
+                    // If slug is /search bump this menu up higher with top attribute
+                    slug === 'search' ? 
+                        'absolute lg:hidden z-50 left-0 w-screen h-fit bg-black transition-all ease-in-out duration-400 py-12' 
+                    :
+                        'absolute lg:hidden z-50 top-20 left-0 w-screen h-fit bg-black transition-all ease-in-out duration-400 py-12'
+                :
+                    'absolute lg:hidden w-screen z-50 h-0 bg-black transition-all ease-in-out duration-400'
+                }
+        >
+                {/* Mobile search - beneath main nav on mobile viewports, unless slug is /search */}
+                <div className={mobileSearchInput ? 'block' : 'hidden'}>
+                    <div>
+                        <label className='text-smart-blue flex lg:hidden justify-center items-center w-full h-24 bg-neutral-900  px-6 md:px-10'>
+                            <SearchForm
+                                searchQuery={ searchQuery }
+                                setSearchQuery={ onSetSearchQuery }
+                                handleSearchFormSubmit={ onHandleSearchFormSubmit } 
+                                mode={'dark'}
+                            />
+                        </label>
+                    </div>
+                </div>
+                {menu.map((el) => (
+                    <div className={mobileNav ? 'px-6 bg-black ' : 'hidden'} key={el.id}>
+                        <Accordion primary={el.name} secondary={el.menuItems.nodes} onToggleNav={onToggleMobileNav}/>
+                    </div>
+                ))}
+                <div
+                    onClick={() => {onSetMobileNav(false), onSetModalOpen(true)}} 
+                    className={mobileNav ? 'text-smart-blue px-6 text-2xl mt-6 cursor-pointer' : 'hidden'}
+                >
+                    Subscribe
+                </div>
+                <div  className={mobileNav ? 'text-smart-blue px-6 text-2xl mt-6 cursor-pointer' : 'hidden'}>
+                    <a href={'/contact-us'}>
+                        Contact Us
+                    </a>
+                </div>
+            </div>
+    )
+}
