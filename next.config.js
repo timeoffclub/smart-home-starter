19dea0f9419999c676ac9831a5c55122b25c00e2
jadamconnor <j.adam.connor@gmail.com>
2022-01-13 17:51:06 -0600

    revert csp header

--- next.config.js
+++ next.config.js
@@ -2,25 +2,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
 
-const securityHeaders = [
-    {
-        key: 'Content-Security-Policy',
-        value: 'default-src'
-    }
-]
-
 module.exports = withBundleAnalyzer({
     reactStrictMode: true,
     images: {
         domains: ['localhost', 'shsprods.wpengine.com'],
-    },
-    async headers() {
-        return [
-            {
-                // Apply these headers to all routes in your application.
-                source: '/(.*)',
-                headers: securityHeaders,
-            },
-        ]
-    },
+    }
 })
