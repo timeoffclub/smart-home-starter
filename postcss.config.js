0022ae4ed1189d3893abdba1ebd9d95103029e18
jadamconnor <j.adam.connor@gmail.com>
2022-01-13 17:05:18 -0600

    experimenting with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,20 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+        ...(process.env.NODE_ENV === 'production' ? { 
+            cssnano: {},
+            purgecss: {
+                // the paths to all template files
+                content: [
+                    "./pages/**/*.{js,jsx,ts,tsx}",
+                    "./components/**/*.{js,jsx,ts,tsx}",
+                ],
+                // function used to extract class names from the templates
+                defaultExtractor: (content) =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+        } : {
+
+        })
     },
 }
