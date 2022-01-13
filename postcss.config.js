50e5c5d5dac5cb9f15f9f721518557471b2d5896
jadamconnor <j.adam.connor@gmail.com>
2022-01-13 13:19:14 -0600

    experimenting with cssnano for minification of css

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
 module.exports = {
     plugins: [
-        "tailwindcss",
+        'tailwindcss',
         [
-            "@fullhuman/postcss-purgecss",
-            process.env.NODE_ENV === "production"
+            '@fullhuman/postcss-purgecss',
+            process.env.NODE_ENV === 'production'
             ? {
                 // the paths to all template files
                 content: [
-                    "./pages/**/*.{js,jsx,ts,tsx}",
-                    "./components/**/*.{js,jsx,ts,tsx}",
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}',
                 ],
                 // function used to extract class names from the templates
                 defaultExtractor: (content) =>
@@ -16,5 +16,6 @@ module.exports = {
             }
             : false,
         ],
+        process.env.NODE_ENV === 'production' && 'cssnano'
     ],
 }
