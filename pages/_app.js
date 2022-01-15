b1076bed95b6f4602e914d6c123069d0e198089d
jadamconnor <j.adam.connor@gmail.com>
2022-01-15 12:58:51 -0600

    removed webVitalsReport

--- pages/_app.js
+++ pages/_app.js
@@ -10,33 +10,33 @@ import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 
-export function reportWebVitals({ id, name, label, value }) {
-    console.log(name, value, id)
-    // Use `window.gtag` if you initialized Google Analytics as this example:
-    // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
-    if (window && window.gtag) {
-        window.gtag('event', name, {
-            event_category:
-                label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-            value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-            event_label: id, // id unique to current page load
-            non_interaction: true, // avoids affecting bounce rate.
-        })
-    }
-}
+// export function reportWebVitals({ id, name, label, value }) {
+//     console.log(name, value, id)
+//     // Use `window.gtag` if you initialized Google Analytics as this example:
+//     // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
+//     if (window && window.gtag) {
+//         window.gtag('event', name, {
+//             event_category:
+//                 label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+//             value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+//             event_label: id, // id unique to current page load
+//             non_interaction: true, // avoids affecting bounce rate.
+//         })
+//     }
+// }
 
 
 function MyApp({ Component, pageProps }) {
-    const router = useRouter()
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url)
-        }
-        router.events.on('routeChangeComplete', handleRouteChange)
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange)
-        }
-    }, [router.events])
+    // const router = useRouter()
+    // useEffect(() => {
+    //     const handleRouteChange = (url) => {
+    //         gtag.pageview(url)
+    //     }
+    //     router.events.on('routeChangeComplete', handleRouteChange)
+    //     return () => {
+    //         router.events.off('routeChangeComplete', handleRouteChange)
+    //     }
+    // }, [router.events])
 
     return (
         <>
