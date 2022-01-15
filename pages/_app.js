a44fa46d37feec990dfca973c3df05cd9d674fcc
jadamconnor <j.adam.connor@gmail.com>
2022-01-15 13:03:43 -0600

    removed webVitalsReport

--- pages/_app.js
+++ pages/_app.js
@@ -27,16 +27,16 @@ import * as gtag from '../lib/gtag'
 
 
 function MyApp({ Component, pageProps }) {
-    // const router = useRouter()
-    // useEffect(() => {
-    //     const handleRouteChange = (url) => {
-    //         gtag.pageview(url)
-    //     }
-    //     router.events.on('routeChangeComplete', handleRouteChange)
-    //     return () => {
-    //         router.events.off('routeChangeComplete', handleRouteChange)
-    //     }
-    // }, [router.events])
+    const router = useRouter()
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
 
     return (
         <>
