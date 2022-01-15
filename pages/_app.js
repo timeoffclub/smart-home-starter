662f32496753d3bb778b3db9d8d8431375d7bfd5
jadamconnor <j.adam.connor@gmail.com>
2022-01-14 20:44:28 -0600

    switched to universal analytics

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export function reportWebVitals({ id, name, label, value }) {
     if (window && window.gtag) {
         window.gtag('event', name, {
             event_category:
-            label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+                label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
             value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
             event_label: id, // id unique to current page load
             non_interaction: true, // avoids affecting bounce rate.
