1fa2b60ef8514627376ce4178597125392bee086
jadamconnor <j.adam.connor@gmail.com>
2022-01-17 23:33:14 -0600

    removed cssnano, postcss, bundle analyzer, and all font-awesome

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
+module.exports = {
+    reactStrictMode: true,
     images: {
         domains: ['localhost', 'shsprods.wpengine.com', 'i0.wp.com'],
     }
-})
+}
