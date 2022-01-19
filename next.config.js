efd0300d34bd0d7b9a600bf385a11239ebb3a36e
jadamconnor <j.adam.connor@gmail.com>
2022-01-18 21:39:42 -0600

    no crawl for staging (preview)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+
 module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['localhost', 'shsprods.wpengine.com', 'i0.wp.com'],
+    }
+}
+
+
+webpack: (config, options) => {
+    // Generate disallow all robots.txt and add to /public folder in preview
+    if (options.isServer && process.env.VERCEL_ENV === 'preview') {
+      require('./scripts/generate-robotstxt-preview.js');
     }
 }
