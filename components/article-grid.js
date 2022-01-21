21e95936328f4ed648f3356ef1ae67cfa01f798a
jadamconnor <j.adam.connor@gmail.com>
2022-01-21 13:19:14 -0600

    missed links on article grid

--- components/article-grid.js
+++ components/article-grid.js
@@ -26,7 +26,7 @@ export default function ArticleGrid ({ myArticles, myCategory }) {
                             }
                             <div>
                                 <div className='text-lg mb-3 hover:underline decoration-1 underline-offset-4 decoration-gray-300 font-semibold'>
-                                    <a href={`../${el.slug}`}>
+                                    <a href={`/${el.slug}`}>
                                         {el.title}
                                     </a>
                                 </div>
