034d050529502a5a40bc445cb6f4f0b228276abc
jadamconnor <j.adam.connor@gmail.com>
2022-01-22 02:42:48 -0600

    image url on article grid incorrect

--- components/article-grid.js
+++ components/article-grid.js
@@ -9,7 +9,7 @@ export default function ArticleGrid ({ myArticles, myCategory }) {
                     {myArticles.map((el, index) => (
                         <div className='h-80' key={el.id}>
                             {el.featuredImage &&
-                                <a href={`../${el.slug}`}>
+                                <a href={`/${el.slug}`}>
                                     <div className='relative h-44 mb-3'>
                                         <Image 
                                             className='transition-all ease-in duration-500'
