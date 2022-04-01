7a9a8b4f0990f9a9935884d961acb174d8c7f30d
jadamconnor <j.adam.connor@gmail.com>
2022-04-01 15:39:11 -0500

    troubleshooting adthrive

--- components/schema.js
+++ components/schema.js
@@ -34,6 +34,26 @@ export default function Schema({ post }) {
                     content={sourceUrl}
                 />
             }
+            <script
+                id='load-ads'
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w, d) {
+                        w.adthrive = w.adthrive || {};
+                        w.adthrive.cmd = w.adthrive.cmd || [];
+                        w.adthrive.plugin = 'adthrive-ads-manual';
+                        w.adthrive.host = 'ads.adthrive.com';
+                    
+                        var s = d.createElement('script');
+                        s.async = true;
+                        s.referrerpolicy='no-referrer-when-downgrade';
+                        s.src = 'https://' + w.adthrive.host + '/sites/6164a6ff014ece4bc4e34c23/ads.min.js?referrer=' + w.encodeURIComponent(w.location.href) + '&cb=' + (Math.floor(Math.random() * 100) + 1);
+                        var n = d.getElementsByTagName('script')[0];
+                        n.parentNode.insertBefore(s, n);
+                    })(window, document);
+                    `,
+                }}
+            />
             {post.productReviewFields.productReview &&
                 <script 
                     type="application/ld+json" 
--- pages/_app.js
+++ pages/_app.js
@@ -44,27 +44,6 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <>
-            <Script
-                id='load-ads'
-                strategy='afterInteractive'
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(w, d) {
-                        w.adthrive = w.adthrive || {};
-                        w.adthrive.cmd = w.adthrive.cmd || [];
-                        w.adthrive.plugin = 'adthrive-ads-manual';
-                        w.adthrive.host = 'ads.adthrive.com';
-                    
-                        var s = d.createElement('script');
-                        s.async = true;
-                        s.referrerpolicy='no-referrer-when-downgrade';
-                        s.src = 'https://' + w.adthrive.host + '/sites/6164a6ff014ece4bc4e34c23/ads.min.js?referrer=' + w.encodeURIComponent(w.location.href) + '&cb=' + (Math.floor(Math.random() * 100) + 1);
-                        var n = d.getElementsByTagName('script')[0];
-                        n.parentNode.insertBefore(s, n);
-                    })(window, document);
-                    `,
-                }}
-            />
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             <Script
                 strategy="afterInteractive"
